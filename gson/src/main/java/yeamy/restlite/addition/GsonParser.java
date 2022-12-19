328fc9eb2f5dd402410b7583653330bc6be9cb56
Yeamy0754 <yeamy0754@hotmail.com>
2022-12-20 00:11:27 +0800

    undo

---
+++ gson/src/main/java/yeamy/restlite/addition/GsonParser.java
@@ -0,0 +1,38 @@
+package yeamy.restlite.addition;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+import yeamy.restlite.RESTfulRequest;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class GsonParser {
+    private static final DateFormat DF = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateFormat TF = new SimpleDateFormat("HH:mm:ss");
+    public static volatile Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss X")
+            .registerTypeAdapter(BigDecimal.class, (JsonSerializer<BigDecimal>)
+                    (src, typeOfSrc, context) -> new JsonPrimitive(src.toPlainString()))
+            .registerTypeAdapter(Date.class, (JsonSerializer<Date>)
+                    (src, typeOfSrc, context) -> new JsonPrimitive(DF.format(src)))
+            .registerTypeAdapter(Time.class, (JsonSerializer<Time>)
+                    (src, typeOfSrc, context) -> new JsonPrimitive(TF.format(src)))
+            .create();
+
+    public static void setGson(Gson gson) {
+        GsonParser.gson = gson;
+    }
+    public static <T> T parse(RESTfulRequest request, Class<T> clz) {
+        return gson.fromJson(request.getBodyAsText(), clz);
+    }
+
+    public static String toJSON(Object data) {
+        return gson.toJson(data);
+    }
+}
--- gson/src/main/java/yeamy/restlite/addition/GsonResponse.java
+++ gson/src/main/java/yeamy/restlite/addition/GsonResponse.java
@@ -1,32 +1,11 @@
 package yeamy.restlite.addition;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializer;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.math.BigDecimal;
-import java.sql.Date;
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 public class GsonResponse extends AbstractHttpResponse<Object> {
-
-    private static final DateFormat DF = new SimpleDateFormat("yyyy-MM-dd");
-    private static final DateFormat TF = new SimpleDateFormat("HH:mm:ss");
-    public static final Gson gson = new GsonBuilder()
-            .setDateFormat("yyyy-MM-dd HH:mm:ss X")
-            .registerTypeAdapter(BigDecimal.class, (JsonSerializer<BigDecimal>)
-                    (src, typeOfSrc, context) -> new JsonPrimitive(src.toPlainString()))
-            .registerTypeAdapter(Date.class, (JsonSerializer<Date>)
-                    (src, typeOfSrc, context) -> new JsonPrimitive(DF.format(src)))
-            .registerTypeAdapter(Time.class, (JsonSerializer<Time>)
-                    (src, typeOfSrc, context) -> new JsonPrimitive(TF.format(src)))
-            .create();
 
     public GsonResponse(Object data) {
         this(200, data);
@@ -39,7 +18,7 @@ public class GsonResponse extends AbstractHttpResponse<Object> {
     }
 
     protected String toJSON() {
-        return gson.toJson(getData());
+        return GsonParser.toJSON(getData());
     }
 
     @Override
