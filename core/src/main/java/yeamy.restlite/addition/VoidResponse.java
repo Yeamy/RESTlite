acc533c37cb0758f1a69aa6596dc8f8eaba9f1e5
Yeamy0754 <yeamy0754@hotmail.com>
2023-01-25 15:08:00 +0800

    Update VoidResponse.java

--- core/src/main/java/yeamy.restlite/addition/VoidResponse.java
+++ core/src/main/java/yeamy.restlite/addition/VoidResponse.java
@@ -1,25 +1,25 @@
 package yeamy.restlite.addition;
 
-import java.io.IOException;
-
 import jakarta.servlet.http.HttpServletResponse;
-
 import yeamy.restlite.HttpResponse;
 
+import java.io.IOException;
+
 public class VoidResponse implements HttpResponse {
-	private final int status;
+    public final static VoidResponse instance = new VoidResponse();
+    private final int status;
 
-	public VoidResponse() {
-		this(200);
-	}
+    public VoidResponse() {
+        this(200);
+    }
 
-	public VoidResponse(int status) {
-		this.status = status;
-	}
+    public VoidResponse(int status) {
+        this.status = status;
+    }
 
-	@Override
-	public void write(HttpServletResponse resp) throws IOException {
-		resp.setStatus(status);
-	}
+    @Override
+    public void write(HttpServletResponse resp) throws IOException {
+        resp.setStatus(status);
+    }
 
 }
