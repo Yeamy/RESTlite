aa9acab686ab82cd9bd1e7da036cad433984712f
Yeamy0754 <yeamy0754@hotmail.com>
2023-01-28 06:07:28 +0800

    example

---
+++ example/src/main/java/bean/InjectParam.java
@@ -0,0 +1,15 @@
+package bean;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class InjectParam implements Closeable {
+
+//    public InjectParam() throws IOException {
+//    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
--- example/src/main/java/example/ExampleMain.java
+++ example/src/main/java/example/ExampleMain.java
@@ -18,29 +18,34 @@ public class ExampleMain {
     InjectCreator creator;
 
 	@GET
-	public String get(@Param(required = false) String p) {
+	public String get(@Inject InjectParam ij, String p) {
 		return "get";
 	}
 
-    @POST
-    public String post1(@Body(creator = "yeamy.restlite.addition.GsonParser") ExampleBody bean) {
-        return null;
-    }
+//	@GET
+//	public String get(@Param(required = false) String p) {
+//		return "get";
+//	}
 
-    @POST
-    public String post2(@Param String p1, @Cookies String c, @Header String h, @GsonBody int b2) {
-        return null;
-    }
+//    @POST
+//    public String post1(@Body(creator = "yeamy.restlite.addition.GsonParser") ExampleBody bean) {
+//        return null;
+//    }
+
+//    @POST
+//    public String post2(@Param String p1, @Cookies String c, @Header String h, @GsonBody int b2) {
+//        return null;
+//    }
 
 //    @POST
 //    public String post3(@Param String p1) {
 //        return null;
 //    }
 
-    @POST
-    public ArrayList<String> post3(String p1, String p2) {
-        return null;
-    }
+//    @POST
+//    public ArrayList<String> post3(String p1, String p2) {
+//        return null;
+//    }
 
     @ERROR
     public Object error(Exception e) {
